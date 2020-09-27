5bdade9cd7ef71a41d88ca58186b6d5616e225ce
rodchen <rodchen_king@outlook.com>
2020-09-27 10:33:55 +0800

    1. 替换combineReducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,8 +2,16 @@ import { combineReducers } from 'redux'
 import todos from './todos'
 import visibilityFilter from './visibilityFilter'
 
-export default combineReducers({
-  todos,
-  visibilityFilter,
-  name: (preState = "", action) => {return preState}
-})
+// export default combineReducers({
+//   todos,
+//   visibilityFilter,
+//   name: (preState = "", action) => {return preState}
+// })
+
+export default function(state = {}, action) {
+  return {
+    todos: todos(state.todos, action),
+    visibilityFilter: visibilityFilter(state.visibilityFilter, action),
+    name: ((preState = "", action) => {return preState})(state.name, action)
+  }
+}
